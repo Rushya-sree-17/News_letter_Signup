e40b86933447f68eae74b9df6f66689461a3e9f5
Rushya <65214110+Rushya-sree-17@users.noreply.github.com>
2020-05-26 23:43:57 +0530

    My first commit of this project

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res) {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  console.log(firstName);
+  console.log(lastName);
+  const email = req.body.email;
+  const data = {
+    members: [{
+      email_address: req.body.email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  }
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us18.api.mailchimp.com/3.0/lists/37e3b0343d";
+  const options = {
+    method: "POST",
+    auth: "rushya:key"
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode == 200) {
+
+      res.sendFile(__dirname + "/success.html");
+    } else {
+
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData);
+  request.end();
+})
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+})
+
+app.listen( process.env.PORT || 3000, function() {
+  console.log("listening on port 3000");
+})
